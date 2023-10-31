636948c5a9fa3d015a28f8bccdf9352728fa0388
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-10-31 21:12:34 +0700

    WriteLine variants

--- include/sharp/console.hpp
+++ include/sharp/console.hpp
@@ -7,6 +7,9 @@ namespace System
     {
     public:
         static void Write(Char value);
-        static void Write(Char value[]);
+        static void Write(Char buffer[]);
+
+        static void WriteLine(Char value);
+        static void WriteLine(Char buffer[]);
     };
 }
--- src/console.cpp
+++ src/console.cpp
@@ -13,3 +13,15 @@ void Console::Write(Char buffer[])
     while (*buffer)
         write(1, buffer++, 2);
 }
+
+void Console::WriteLine(Char value)
+{
+    Write(value);
+    write(1, "\n", 1);
+}
+
+void Console::WriteLine(Char buffer[])
+{
+    Write(buffer);
+    write(1, "\n", 1);
+}
