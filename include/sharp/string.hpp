3bdd0a81c7dd54eb2e1aea8ae6ef6033a07319bf
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-01 08:01:37 +0700

    Default constructor and destructor for String

--- include/sharp/string.hpp
+++ include/sharp/string.hpp
@@ -1,8 +1,16 @@
 #pragma once
+#include <sharp/char.hpp>
 
 namespace System
 {
     class String
     {
+    private:
+        Char *_chars;
+    public:
+        int Length = 0;
+
+        String();
+        ~String();
     };
 }
--- src/string.cpp
+++ src/string.cpp
@@ -1,3 +1,14 @@
 #include <sharp/string.hpp>
 
 using namespace System;
+
+String::String()
+{
+    _chars = new Char[1];
+    _chars[0] = '\0';
+}
+
+String::~String()
+{
+    delete[] _chars;
+}
